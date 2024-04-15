3664bc5a91b9bb551669595bf472c78b05ebe9a6
Aliu Tijani <aliutijani21@gmail.com>
2024-04-15 11:27:36 +0000

    text generation with gpt-3.5-turbo

---
+++ generateText.py
@@ -0,0 +1,34 @@
+# import the OpenAI Python library for calling the OpenAI API
+from openai import OpenAI
+import os
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "<your OpenAI API key if not set as env var>"))
+
+# Example OpenAI Python library request
+MODEL = "gpt-3.5-turbo"
+# response = client.chat.completions.create(
+#     model=MODEL,
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": "Knock knock."},
+#         {"role": "assistant", "content": "Who's there?"},
+#         {"role": "user", "content": "Orange."},
+#     ],
+#     temperature=0,
+# )
+
+# response.choices[0].message.content
+
+# example with a system message
+response = client.chat.completions.create(
+    model=MODEL,
+    # Use messages as prompt
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Explain asynchronous programming in the style of the pirate Blackbeard."},
+    ],
+    temperature=0,
+)
+
+print(response.choices[0].message.content)
+
