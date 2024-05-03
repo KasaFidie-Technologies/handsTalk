3e377a01934b151ad5213bf65a8194fa461ae1da
ken BED <you@example.com>
2024-05-03 09:58:25 +0000

    commit

---
+++ SignGPT/README.md
@@ -0,0 +1,24 @@
+# sign-language-detector-python
+
+Sign language detector with Python, OpenCV and Mediapipe !
+Respons from chatGPT 3.5
+
+
+Team: GeekTeam hackathon group
+Ezra
+Enoch Djam Beamahn
+
+To run:
+cd to directory
+
+run this in the terminal to activate venv.
+source venv/bin/activate
+
+run the signGPT.py
+
+
+
+NOTE
+the text edit must not be empty else the camera will crach
+
+I will repair that later
---
+++ SignGPT/signGPT.py
@@ -0,0 +1,148 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget, QTextEdit, QHBoxLayout, QTextBrowser, QPushButton
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import QTimer
+import cv2
+import mediapipe as mp
+import pickle
+import numpy as np
+from chatGPT import respond
+
+
+class HandDetectionApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.video_capture = cv2.VideoCapture(0)  # Access the default camera (index 0)
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands()
+        
+        self.image_label = QLabel(self)
+        self.text_edit = QTextBrowser(self)  # Create a QTextEdit widget
+        self.text_edit1 = QPushButton("send")  # Create a QTextEdit widget
+        self.textfield = QTextEdit(self)
+
+        
+        master_layout = QHBoxLayout()
+
+        col1 = QVBoxLayout()
+        col2 = QVBoxLayout ()
+
+
+        col1.addWidget(self.text_edit, 50)  # Add the QTextEdit widget to the layout below the QLabel
+        col1.addWidget(self.text_edit1)
+        col2.addWidget(self.image_label, 70)
+        col2.addWidget(self.textfield)
+
+
+        master_layout.addLayout(col1, 40)
+        master_layout.addLayout(col2, 60)
+
+        self.setLayout(master_layout)
+
+       
+        self.text_edit1.clicked.connect(self.get_text) 
+        self.text_edit1.clicked.connect(self.get_text) 
+        
+        
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start()  # Update every 30 milliseconds
+
+        self.setGeometry(100, 100, 800, 600) 
+
+        self.sentence = []  # Define sentence list outside update_frame method
+        self.word = ['']
+        self.cursor = ['|']
+        self.output = ''
+
+        self.text_edit1.clicked.connect(self.update_frame)
+        # print(self.update_frame())
+
+    def update_frame(self):
+        model_dict = pickle.load(open('./model.p', 'rb'))
+        model = model_dict['model']
+        mp_hands = mp.solutions.hands
+        mp_drawing = mp.solutions.drawing_utils
+        mp_drawing_styles = mp.solutions.drawing_styles
+
+        labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
+
+
+        data_aux = []
+        x_ = []
+        y_ = []
+
+        ret, frame = self.video_capture.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        H, W, _ = frame.shape
+
+        results = self.hands.process(frame)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,  # image to draw
+                    hand_landmarks,  # model output
+                    mp_hands.HAND_CONNECTIONS,  # hand connections
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+
+                    x_.append(x)
+                    y_.append(y)
+
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
+
+            x1 = int(min(x_) * W) - 10
+            y1 = int(min(y_) * H) - 10
+
+            x2 = int(max(x_) * W) - 10
+            y2 = int(max(y_) * H) - 10
+
+            prediction = model.predict([np.asarray(data_aux)])
+
+            predicted_character = labels_dict[int(prediction[0])]
+            self.sentence.append(predicted_character)
+
+            if len(self.sentence) > 10: 
+                self.sentence = self.sentence[-10:]
+                if predicted_character != self.word[-1]:
+                    current_text = self.textfield.toPlainText()
+                    self.sentence.clear()
+                    if current_text == '':
+                        self.textfield.setText(predicted_character)
+                    if predicted_character != current_text[-1]:  # Check if the predicted character is different from the last appended character
+                        updated_text = current_text + predicted_character
+                        self.textfield.setText(updated_text)
+        
+
+            cv2.rectangle(frame, (0,0), (640, 40), (245, 117, 16), -1)
+            cv2.putText(frame, ' '.join(self.sentence), (3,30), 
+                                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            
+
+        image = QImage(frame, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+        self.image_label.setPixmap(QPixmap.fromImage(image))
+
+        return self.word
+    
+
+    def get_text(self):
+        text = self.textfield.toPlainText()  # Retrieve text from QTextEdit widget
+        self.result = respond(text)
+        self.text_edit.setText(self.result)
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    hand_detection_app = HandDetectionApp()
+    hand_detection_app.show()
+    sys.exit(app.exec_())
